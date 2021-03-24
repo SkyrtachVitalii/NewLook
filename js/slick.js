5c6ca4146ee6da4c4eaa24428a9c221a353ff828
Vitalii Skyrtach <vitalijskirtac@gmail.com>
2021-03-24 19:31:19 +0200

    first commit

---
+++ js/slick.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.infoSlider').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        infinite: true,
+        dots: true,
+        swipe: true,
+        swipeToSlide: true
+    });
+  });
